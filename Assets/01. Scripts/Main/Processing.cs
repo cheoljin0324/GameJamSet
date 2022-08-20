c4298027bcf7bbfe30b2b7df236771dfa682f181
SEH00N <98886309+SEH00N@users.noreply.github.com>
2022-08-21 06:56:05 +0900

    asset apply

--- Assets/01. Scripts/Main/Processing.cs
+++ Assets/01. Scripts/Main/Processing.cs
@@ -10,10 +10,12 @@ public class Processing : MonoBehaviour
     public Button button = null;
     private Image image = null;
     private MakingCoolTime ctime = null;
+    private GameObject workingPanel = null;
     public int Index { get; set; } = 0;
 
     private void Awake()
     {
+        workingPanel = transform.parent.parent.parent.Find("WorkingPanel").gameObject;
         ctime = transform.parent.GetComponent<MakingCoolTime>();
         button = GetComponent<Button>();
         image = GetComponent<Image>();
@@ -27,6 +29,7 @@ public class Processing : MonoBehaviour
 
     public void DoProcessing()
     {
+        if(workingPanel.activeSelf) return;
         if(JewelryManager.Instance.haveJewelry[count] <= 0)
         {
             TextPrefab temp = PoolManager.Instance.Pop("TextPrefab") as TextPrefab;
