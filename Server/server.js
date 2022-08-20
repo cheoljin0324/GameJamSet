ecfff2540112012a3fdc69e21100461e53a9b79b
kwak1s1h <kwak1s1h@kakao.com>
2022-08-20 15:10:32 +0900

    server

---
+++ Server/server.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const ws = require('ws');
+const wss = new ws.Server({port:3000});
+
+const path = './data.json';
+
+wss.on('listening', () => {
+    console.log(`server opened on port ${wss.options.port}`);
+});
+
+wss.on('connection', client => {
+    client.on('message', msg => {
+        const file = require('./data.json');
+        const data = JSONParse(msg.toString());
+        if(data == false) return;
+        if((data.n == undefined) || (data.f == undefined)) return;
+        if(file.length <= 0)
+        {
+            file.push(data);
+            fs.writeFileSync(path, JSON.stringify(file));
+            return;
+        }
+        let find = false;
+        file.forEach((userData, index) => {
+            if(userData.n == data.n)
+            {
+                file[index] = data;
+                fs.writeFileSync(path, JSON.stringify(file));
+                find = true;
+            }
+        });
+        if(!find)
+        {
+            file.push(data);
+            fs.writeFileSync(path, JSON.stringify(file));
+        }
+        client.send(JSON.stringify(file));
+    });
+});
+
+const JSONParse = stringData => {
+    try
+    {
+        return JSON.parse(stringData);
+    }
+    catch(err)
+    {
+        return false;
+    }
+}
